cc3c0531188a063cd893ebbb4585de330d8280a2
Kapatsila_Mariia <m.kapatsila@gmail.com>
2022-02-12 18:31:19 +0200

    Add confirmation email

--- server/config/application.rb
+++ server/config/application.rb
@@ -30,7 +30,8 @@ module AdvertApp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Kyiv"
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
