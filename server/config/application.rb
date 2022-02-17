309c71d8f1f0fb664e94691c2fe8b4c89a0d0b63
AlexDashko1337 <dashkosanya@gmail.com>
2022-02-17 17:12:01 +0200

    Fixed ukrainian locale

--- server/config/application.rb
+++ server/config/application.rb
@@ -22,9 +22,9 @@ Bundler.require(*Rails.groups)
 
 module AdvertApp
   class Application < Rails::Application
-    config.i18n.available_locales = %i[en ru ukr]
+    config.i18n.available_locales = %i[en ru uk]
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :en
 
 
     config.load_defaults 7.0
