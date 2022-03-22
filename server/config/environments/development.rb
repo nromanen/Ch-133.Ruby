01c8aefea386a6fb419a3f6df3095709218dfd2f
AlexDashko1337 <88824574+AlexDashko1337@users.noreply.github.com>
2022-03-22 12:35:24 +0200

    Update development.rb

--- server/config/environments/development.rb
+++ server/config/environments/development.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: "advertisements.smtp@gmail.com",
-    password: "Advert-smtp",
+    user_name: "#{ENV["SMTP_EMAIL"]}",
+    password: "#{ENV["SMTP_EMAIL_PASSWORD"]}",
     domain: "localhost:3000",
     address: "smtp.gmail.com",
     port: "587",
