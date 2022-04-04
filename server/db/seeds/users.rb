9fb1562d2776dfc3d634561086fc12ad49969125
le-murier <vofimovic@gmail.com>
2022-04-04 17:05:13 +0100

    delete query

--- server/db/seeds/users.rb
+++ server/db/seeds/users.rb
@@ -1,26 +1,21 @@
 connection = ActiveRecord::Base.connection()
 @adminRole = Role.where(name: 'Admin').first&.id
+admin_nick = 'admin'
+user_nick = 'seedUser'
 
 User.create!(
   password: ENV["USER_PASSWORD"],
   email: ENV["USER_EMAIL"],
-  nick_name: "seedUser",
+  nick_name: user_nick,
   password_confirmation: ENV["USER_PASSWORD"]
 )
 
 User.create!(
   password: ENV["ADMIN_PASSWORD"],
   email: ENV["ADMIN_EMAIL"],
-  nick_name: "admin",
+  nick_name: admin_nick,
   password_confirmation: ENV["ADMIN_PASSWORD"]
 )
 
-comands = [
-  "UPDATE \"users\" SET \"confirmed_at\"='2022-03-22 17:19:40.342358' WHERE \"nick_name\"='seedUser';",
-  "UPDATE \"users\" SET \"confirmed_at\"='2022-03-22 17:19:40.342358' WHERE \"nick_name\"='admin';",
-  "UPDATE \"users\" SET \"role_id\"='#{@adminRole}' WHERE \"nick_name\"='admin';"
-]
-
-comands.each do |i|
-  connection.execute(i)
-end
+User.where(nick_name: admin_nick).first&.update(confirmed_at: 5.days.ago)
+User.where(nick_name: user_nick).first&.update(confirmed_at: 5.days.ago)
