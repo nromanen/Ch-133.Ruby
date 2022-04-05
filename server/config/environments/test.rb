a689cc89a19947058635f9a237693ea37247934a
le-murier <82406343+le-murier@users.noreply.github.com>
2022-04-06 00:10:35 +0100

    Update test.rb

--- server/config/environments/test.rb
+++ server/config/environments/test.rb
@@ -57,7 +57,6 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: "localhost:3000" }
-  config.active_record.migration_error = false
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 end
