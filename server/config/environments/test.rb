7599570c780e2ceb3a49ea580ec46e6dc292d8e5
le-murier <vofimovic@gmail.com>
2022-04-11 22:06:57 +0100

    prepare pull-request

--- server/config/environments/test.rb
+++ server/config/environments/test.rb
@@ -17,6 +17,7 @@ Rails.application.configure do
   # this probably isn't necessary. It's a good idea to do in a continuous integration
   # system, or in some way before deploying your code.
   config.eager_load = ENV["CI"].present?
+
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
