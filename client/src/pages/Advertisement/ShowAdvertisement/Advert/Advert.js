ac8fe7bc1c787131a2413ed04a66f423d02b684d
Kapatsila_Mariia <m.kapatsila@gmail.com>
2022-04-19 16:45:32 +0300

    Fixed components and add comments

--- client/src/pages/Advertisement/ShowAdvertisement/Advert/Advert.js
+++ client/src/pages/Advertisement/ShowAdvertisement/Advert/Advert.js
@@ -1,29 +1,16 @@
-import React, {useState, useEffect} from "react";
-import Message from '../../../../components/toster/message'
-import Cookies from 'universal-cookie';
-import { useParams } from "react-router-dom";
-import {baseShowAdvert} from "../../../../consts";
-//import ImageUploader from "react-images-upload";
-//import { withTranslation } from 'react-i18next';
+import React from "react";
 import "../../../../i18n";
 import './Advert.scss'
-const cookies = new Cookies();
 
 const Advert = (resp) => {
-    let params = useParams();
-    const [image, serImage] = useState('https://image.shutterstock.com/image-vector/question-bubbles-line-icon-ask-260nw-2004744938.jpg')
-    const [messageText, setMessageText] = useState('');
-    const [severity, setSeverity] = useState('');
-    //console.log(resp.title);
 
     return (
         <div className={'post'}>
-            {!!messageText ? <Message text={messageText} type={severity}/> : null}
                 <h2 className={'post'}>{resp.title}</h2>
             <div className={'post-text'}>
                 <h4>{resp.text}</h4>
                 <div className={'post-image'}>
-                    <img src={image} alt="im"></img>
+                    <img src={resp.image_url} alt="im"></img>
                 </div>
             </div>
         </div>
