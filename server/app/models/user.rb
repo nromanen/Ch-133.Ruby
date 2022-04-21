63ce1cc79896e841d128092e2ddd984f8303810f
le-murier <82406343+le-murier@users.noreply.github.com>
2022-04-21 13:51:12 +0100

    Update user.rb

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -16,8 +16,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :adverts, dependent: :destroy
   has_many :likes, dependent: :destroy
-  belongs_to :role, optional: true 
-  delegate :name, to: :role
+  belongs_to :role
   has_one :user_info, dependent: :destroy
 
   validates :email, presence: true, uniqueness: { uniqueness: true, message: I18n.t("taken") },
@@ -33,7 +32,7 @@ class User < ApplicationRecord
   validates_confirmation_of  :password, message: I18n.t("password_confirm")
   def set_default
     role_user = Role.find_by(name: "User")
-    self.role = role_user
+    self.role_id = role_user.id
   end
 
   def get_advert_count
@@ -41,6 +40,6 @@ class User < ApplicationRecord
   end
 
   def jwt_payload
-    { "email" => self.email, "id" => self.id, "role" => self.name }
+    { "email" => self.email, "id" => self.id }
   end
 end
