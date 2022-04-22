9aa88deab5fa7986114aa82ee44681b280d09e96
le-murier <82406343+le-murier@users.noreply.github.com>
2022-04-22 16:11:44 +0100

    Update users.rb

--- server/db/seeds/users.rb
+++ server/db/seeds/users.rb
@@ -2,12 +2,12 @@ admin_nick = 'admin'
 user_nick = 'seedUser'
 admin_id = Role.where(name: 'Admin').first&.id
 
-User.create!(
-  password: ENV["ADMIN_PASSWORD"],
-  email: ENV["ADMIN_EMAIL"],
-  nick_name: admin_nick,
-  password_confirmation: ENV["ADMIN_PASSWORD"]
-);
-
-User.where(nick_name: admin_nick).first&.update_columns(role_id: admin_id, confirmed_at: Time.now.utc)
-User.where(nick_name: user_nick).first&.update_attribute(:confirmed_at, Time.now.utc);
+if ENV["ADMIN_PASSWORD"] && ENV["ADMIN_EMAIL"]
+  User.create!(
+    password: ENV["ADMIN_PASSWORD"],
+    email: ENV["ADMIN_EMAIL"],
+    nick_name: admin_nick,
+    password_confirmation: ENV["ADMIN_PASSWORD"]
+  );
+  User.where(nick_name: admin_nick).first&.update_columns(role_id: admin_id, confirmed_at: Time.now.utc)
+end
