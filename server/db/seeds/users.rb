e28cb12b4ed0d2d1a35d609e5467272d848292d6
le-murier <82406343+le-murier@users.noreply.github.com>
2022-04-22 16:30:34 +0100

    Update users.rb

--- server/db/seeds/users.rb
+++ server/db/seeds/users.rb
@@ -2,7 +2,7 @@ admin_id = Role.where(name: 'Admin').first&.id
 admin = User.new(
   password: ENV["ADMIN_PASSWORD"],
   email: ENV["ADMIN_EMAIL"],
-  nick_name: admin_nick,
+  nick_name: "admin",
   password_confirmation: ENV["ADMIN_PASSWORD"]
 );
 
