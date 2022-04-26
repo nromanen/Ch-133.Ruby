0f08066eabca99c92069a6ff6f193a13bef8df88
le-murier <82406343+le-murier@users.noreply.github.com>
2022-04-26 14:04:53 +0100

    Update user.rb

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  include Rails.application.routes.url_helpers
   before_validation :set_default
   
   # Include default devise modules. Others available are:
