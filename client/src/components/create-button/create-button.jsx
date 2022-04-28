8e6b965a962fa15e5457f80dc7b05385c0c5b938
Andrii-Dm <andy29229@gmail.com>
2022-04-28 13:14:59 +0300

    Fixed translation for Adverts front

--- client/src/components/create-button/create-button.jsx
+++ client/src/components/create-button/create-button.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import '../create-button/create-button.scss';
 import {useNavigate} from "react-router-dom";
+import { withTranslation } from 'react-i18next';
+import "../../i18n";
 
 const CreateButton = (props) => {
     let navigate = useNavigate();
+    const { t } = props;
     return(
     <button className='create-button' onClick={() => {navigate(props.link, {replace: true})}}>
-        <span> Create an advert </span>
+        <span> {t("Advert.createButton")} </span>
     </button>
 )
 }
-export default  CreateButton;
+export default  withTranslation()(CreateButton);
